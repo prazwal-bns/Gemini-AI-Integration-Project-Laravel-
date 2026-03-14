d9a1fa4a7f3be999ea88cb24b64209f730d2ddeb
prazwal-bns <prajwalbns15@gmail.com>
2026-03-14 22:45:31 +0545

    Implement static method for content generation in GeminiAPIService using Google API

--- app/Services/GeminiAPIService.php
+++ app/Services/GeminiAPIService.php
@@ -2,13 +2,37 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class GeminiAPIService
 {
     /**
-     * Create a new class instance.
+     * @return array<mixed>
      */
-    public function __construct()
+    public static function generateContent(string $prompt): array
     {
-        //
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'X-goog-api-Key' => config('app.gemini_api_key'),
+        ])
+            ->post('https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent', [
+                'contents' => [
+                    [
+                        'parts' => [
+                            [
+                                'text' => $prompt,
+                            ],
+                        ],
+                    ],
+                ],
+            ]);
+
+        if ($response->successful()) {
+            $text = $response->json()['candidates'][0]['content']['parts'][0]['text'];
+        } else {
+            $text = 'Something went wrong';
+        }
+
+        return ['response' => $text];
     }
 }
